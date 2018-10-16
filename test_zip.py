b63311e5389157b178819a49aecbabb18a65eada
yagami light <halysl0817@gmail.com>
2018-10-16 22:34:23 +0800

    add test_zip

---
+++ test_zip.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+print('a={}, b={}, zip(a,b)={}'.format(a, b, zip(a,b)))
+
+a = [1, 2, 3]
+b = [4, 5, 6, 7, 8]
+print('a={}, b={}, zip(a,b)={}'.format(a, b, zip(a,b)))
+
+a = 'abc'
+b = 'def'
+print('a={}, b={}, zip(a,b)={}'.format(a, b, zip(a,b)))
+
+a = 'abc'
+b = 'defghi'
+print('a={}, b={}, zip(a,b)={}'.format(a, b, zip(a,b)))
