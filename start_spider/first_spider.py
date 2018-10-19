a792cc8c6a7c5a48a39412c411e5e9ede1eb57dc
yagami light <halysl0817@gmail.com>
2018-10-19 22:26:46 +0800

    返璞归真，从头开始

---
+++ start_spider/first_spider.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+class HttpConnectError(Exception):
+    pass
+
+
+class UnknowError(Exception):
+    pass
+
+
+def get_info(url):
+    try:
+        r = requests.get(url)
+        if r.status_code == 200:
+            r.encoding = 'utf-8'
+            return r.text
+        else:
+            raise HttpConnectError
+    except UnknowError as e:
+        print(e.message)
+
+
+try:
+    url = 'http://www.baidu.com'
+    result = get_info(url)
+    print(result)
+except HttpConnectError as e:
+    print('连接错误')
