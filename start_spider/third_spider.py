6840e2b9da1c2c3cfaae89e01506cb1af2bf1266
yagami light <halysl0817@gmail.com>
2018-10-20 19:25:05 +0800

    replace 200 to requests.codes.ok

--- start_spider/third_spider.py
+++ start_spider/third_spider.py
@@ -10,7 +10,7 @@ from second_spider import get_text_info, get_json_info
 
 def get_info(url, headers=None, flag=None):
     r = requests.get(url, headers=headers)
-    if r.status_code == 200:
+    if r.status_code == requests.codes.ok:
         r.encoding = 'utf-8'
     else:
         raise exception.HttpConnectError
@@ -27,6 +27,11 @@ def get_info(url, headers=None, flag=None):
 
 if __name__ == "__main__":
     url = 'https://api.github.com/some/endpoint'
+    # 注意: 定制 header 的优先级低于某些特定的信息源，例如：
+    # 如果在 .netrc 中设置了用户认证信息，使用 headers= 设置的授权就不会生效。而如果设置了 auth= 参数，``.netrc`` 的设置就无效了。
+    # 如果被重定向到别的主机，授权 header 就会被删除。
+    # 代理授权 header 会被 URL 中提供的代理身份覆盖掉。
+    # 在我们能判断内容长度的情况下，header 的 Content-Length 会被改写。
     headers = {'user-agent': 'my-app/0.0.1'}
     result = get_info(url, headers=headers)
     print(result)
