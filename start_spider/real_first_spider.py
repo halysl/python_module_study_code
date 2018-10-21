900b640c7c9542c5bda5c1f6b1d734b24f32d6ac
yagami light <halysl0817@gmail.com>
2018-10-21 23:07:49 +0800

    finish get context

--- start_spider/real_first_spider.py
+++ start_spider/real_first_spider.py
@@ -4,3 +4,20 @@
 # info:一个真正，可用的爬虫
 
 # 选择目标：pass
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+def get_info(url, headers=None, flag=None):
+    r = requests.get(url)
+    if r.status_code == requests.codes.ok:
+        return r.text
+    else:
+        return requests.RequestException
+
+
+if __name__ == "__main__":
+    url = 'https://steamcn.com/forum.php'
+    result = get_info(url)
+    print(result)
