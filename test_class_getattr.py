6432aaef7c5ddb76e840778a66166a155309af51
yagami light <halysl0817@gmail.com>
2018-10-31 16:10:39 +0800

    add magic methed class getitem

--- test_class_getattr.py
+++ test_class_getattr.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# @Date    : 2018-10-30 18:37:17
+# @Date    : 2018-10-31 16:02:17
 # @Author  : Light (halysl0817@gmail.com)
 # @Link    : ${link}
 # @Version : $Id$
 # @slogan: 狂风骤雨催纸伞，游人浪迹步不休，天地滂沱如何渡，蓑衣褪尽任浊流。
-# @info: $info$
+# @info: 类的__getattr__ __getattribute__方法
 
 class A(object):
     def __init__(self, name, age):
