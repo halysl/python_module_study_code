a2cb4d1735bdb2e124eb291cb7575f5b77736ce6
yagami light <halysl0817@gmail.com>
2018-10-31 17:51:11 +0800

    add doctest example

---
+++ test_doctest.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Date    : 2018-10-30 18:37:17
+# @Author  : Light (halysl0817@gmail.com)
+# @Link    : ${link}
+# @Version : $Id$
+# @slogan: 狂风骤雨催纸伞，游人浪迹步不休，天地滂沱如何渡，蓑衣褪尽任浊流。
+# @info: $info$
+
+import doctest
+
+def multiply(a, b):
+    """
+    >>> multiply(1, 4)
+    4
+    >>> multiply(0, 2)
+    0
+    >>> multiply(2, 0.5)
+    1.0
+    >>> multiply('a', 3)
+    'aaa'
+    >>> multiply(3, 'a')
+    'aaa'
+    >>> multiply('a', 0)
+    ''
+    """
+    return a * b
+
+if __name__ == "__main__":
+    doctest.testmod(verbose=False)
