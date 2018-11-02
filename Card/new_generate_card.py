32c4778c813fa43ee478701e0365477ad8668050
yagami light <halysl0817@gmail.com>
2018-11-02 15:59:04 +0800

    21点游戏：更为统一的工厂调用方式

---
+++ Card/new_generate_card.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Date    : 2018-10-30 18:37:17
+# @Author  : Light (halysl0817@gmail.com)
+# @Link    : ${link}
+# @Version : $Id$
+# @slogan: 狂风骤雨催纸伞，游人浪迹步不休，天地滂沱如何渡，蓑衣褪尽任浊流。
+# @info: $info$
+
+from new_card import AceCard, NumberCard, FaceCard
+
+def card5(rank, suit):
+    if rank == 1: return AceCard(rank, suit)
+    elif 2 <= rank < 11: return NumberCard(rank, suit)
+    elif 11 <= rank < 14: return FaceCard(rank, suit)
+    else:
+        raise Exception('rank out of index')
