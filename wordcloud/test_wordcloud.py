eabdb388bafefa097d083cd510b7ee67e67de855
yagami light <halysl0817@gmail.com>
2018-11-08 14:33:50 +0800

    pyecharts wordcloud function

---
+++ wordcloud/test_wordcloud.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Date    : 2018-10-30 18:37:17
+# @Author  : Light (halysl0817@gmail.com)
+# @Link    : ${link}
+# @Version : $Id$
+# @slogan: 狂风骤雨催纸伞，游人浪迹步不休，天地滂沱如何渡，蓑衣褪尽任浊流。
+# @info: $info$
+
+from pyecharts import WordCloud
+import json
+name = [] # 词条
+value = [] # 权重
+with open('data.json', 'r') as f:
+    data = json.load(f)
+
+for k, v in data.items():
+    name.append(k)
+    value.append(int(v))
+
+wordcloud = WordCloud(width=1300, height=620)
+wordcloud.add("", name, value, word_size_range=[20, 100])
+wordcloud.render()
