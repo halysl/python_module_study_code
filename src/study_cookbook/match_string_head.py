3afb8136996bc7dee360d02b231689437990313d
yagami light <halysl0817@gmail.com>
2018-12-18 17:31:32 +0800

    cookbook2.2:字符串开头或结尾匹配

---
+++ src/study_cookbook/match_string_head.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+#=============================================================================
+# FileName: 
+# Desc: 字符串开头或结尾匹配 startswith endswith
+# Author: 刘志
+# Email: halysl0817@gmail.com
+# HomePage: ${link}
+# Version: 0.0.1
+# LastChange: 2018-11-28 15:24
+# History:
+# slogan: 狂风骤雨催纸伞，游人浪迹步不休，天地滂沱如何渡，蓑衣褪尽任浊流。
+#=============================================================================
+"""
+
+def match():
+    a = 'data_struct.py'
+    with open(a, 'r') as f:
+        data = f.readlines()
+        for i in data:
+            if i.startswith('#!/usr/bin/env python'):
+                print '--START--', i
+            if i.endswith('"__main__":\n'):
+                print '--MAIN--', i
+
+if __name__ == "__main__":
+    match()
