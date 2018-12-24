54fbf558e19500c4f34340b9755d07dbdd4b2373
yagami light <halysl0817@gmail.com>
2018-12-24 23:29:32 +0800

    up

---
+++ src/study_cookbook/string_replace.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+#=============================================================================
+# FileName: 
+# Desc: 字符串搜索和替换
+# Author: 刘志
+# Email: halysl0817@gmail.com
+# HomePage: ${link}
+# Version: 0.0.1
+# LastChange: 2018-11-28 15:24
+# History:
+# slogan: 狂风骤雨催纸伞，游人浪迹步不休，天地滂沱如何渡，蓑衣褪尽任浊流。
+#=============================================================================
+"""
+import re
+
+def replace_string_with_strmethod():
+    pass
+
+def replace_string_with_resub():
+    pass
+
+if __name__ == "__main__":
+    replace_string_with_strmethod()
+    replace_string_with_resub()
