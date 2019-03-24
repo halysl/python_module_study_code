de68b5b9587d5dfe79a4de68d4f6cacd41d4d8a0
light <halysl0817@gmail.com>
2019-03-24 16:57:29 +0800

    upup

--- src/utils/random_trasport.py
+++ src/utils/random_trasport.py
@@ -1,3 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import random
+
+
+def random_tra(second):
+    li = []
+    mo = second / 100.0
+    for i in range(second):
+        li.append(random.randint(0, 1))
+    for i in range(len(li)):
+        if i > 0:
+            result_pre = i * li[i-1] / mo
+            result_now = i * li[i] / mo
+            if not result_now:
+                result_now = result_pre
+        else:
+            result_now = i * li[i] / mo
+        print("{0}%".format(result_now))
+
+
+if __name__ == "__main__":
+    random_tra(600)
