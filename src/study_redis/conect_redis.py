3860b7c10a7113fe1a6a92733553bc03e7cc592e
yagami light <halysl0817@gmail.com>
2019-04-11 17:48:17 +0800

    add a redis

---
+++ src/study_redis/conect_redis.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import time
+import redis
+
+if __name__ == "__main__":
+    r = redis.Redis(host="localhost", port=6379, decode_responses=True)
+    r.set("a", "b")
+    print(r["a"])
+    print(r.get("a"))
+    print(type(r))
+
+    r.set("a", "1234", ex=3)
+    print(r["a"])
+    time.sleep(1)
+    print(r["a"])
+    time.sleep(3)
+    print(r.get("a"))
