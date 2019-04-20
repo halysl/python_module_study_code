2ecd613468d71a8eae216c0955b469bf6ea96d74
yagami light <halysl0817@gmail.com>
2019-04-20 17:53:13 +0800

    add 数字的四舍五入

---
+++ src/study_cookbook/数字的四舍五入.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+def sishewuru():
+    print(round(1.23, 1))
+    print(round(1.27, 1))
+    print(round(-1.27, 1))
+    print(round(1.25361, 3))
+
+    print(round(1.5))
+    print(round(2.5))
+
+    a = 1627731
+    print(round(a, -1))
+    print(round(a, -2))
+    print(round(a, -3))
+
+    
+if __name__ == "__main__":
+    sishewuru()
