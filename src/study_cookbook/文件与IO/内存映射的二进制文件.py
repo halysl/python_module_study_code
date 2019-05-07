a6890d0c17e0df0f37055ab2707521c20a62e375
yagami light <halysl0817@gmail.com>
2019-05-07 17:10:31 +0800

    up

---
+++ src/study_cookbook/文件与IO/内存映射的二进制文件.py
@@ -0,0 +1,13 @@
+# -*- coding utf-8 -*-
+
+import os
+import mmap
+
+
+def memory_map(filename, access=mmap.ACCESS_WRITE):
+    size = os.path.getsize(filename)
+    fd = os.open(filename, os.O_RDWR)
+    return mmap.mmap(fd, size, access=access)
+
+if __name__ == "__main__":
+    memory_map()
