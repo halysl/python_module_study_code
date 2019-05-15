6c03718871cc93bf4bc9e06dfaa02bba9a51cb9a
yagami light <halysl0817@gmail.com>
2019-05-15 16:46:01 +0800

    add getattr setattr

---
+++ src/study_standard/study_getter.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+class A():
+    attr1 = "test"
+
+    def __init__(self):
+        self.attr2 = "test2"
+        self.attr3 = "test3"
+
+
+def example_getter():
+    a = A()
+    print("a为{}\ntype(a) ==> {}\na.__dict__={}".format(a, type(a), a.__dict__))
+    print("getattr(a, 'attr1') ==> {}".format(getattr(a, "attr1")))
+    print("getattr(a, 'attr2') ==> {}".format(getattr(a, "attr2")))
+    print("getattr(a, 'attr3') ==> {}".format(getattr(a, "attr3")))
+
+
+def example_setter():
+    a = A()
+    print("a为{}\ntype(a) ==> {}\na.__dict__={}".format(a, type(a), a.__dict__))
+    print("将会把attr1、attr2、attr3再次赋值")
+    setattr(a, "attr1", 123)
+    setattr(a, "attr2", 456)
+    setattr(a, "attr3", 789)
+    print("setattr(a, 'attr1', 123) ==> {}".format(a.attr1))
+    print("setattr(a, 'attr2', 456) ==> {}".format(a.attr2))
+    print("setattr(a, 'attr3', 789) ==> {}".format(a.attr3))
+
+if __name__ == "__main__":
+    example_getter()
+    example_setter()
