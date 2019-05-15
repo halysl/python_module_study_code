3b2341e1857d152de3e2e89dae96bd30ea11da44
yagami light <halysl0817@gmail.com>
2019-05-15 16:38:43 +0800

    add write yml

--- src/study_cookbook/数据编码和处理/读取yaml文件.py
+++ src/study_cookbook/数据编码和处理/读取yaml文件.py
@@ -17,6 +17,14 @@ def example_yml_without_waring():
         data = yaml.load(f, Loader=yaml.FullLoader)
     print(data)
 
+
+def example_yml_write():
+    data = {"a": 1, "b": 2, "c": [1, 2, 3], "d": {"e": "test", "f": "test2",
+            "g": [1, 0, 2, 4]}}
+    with open(os.path.join(path, "write_yaml.yml"), "w") as f:
+        yaml.dump(data, f)
+
 if __name__ == "__main__":
     example_yml()
     example_yml_without_waring()
+    example_yml_write()
