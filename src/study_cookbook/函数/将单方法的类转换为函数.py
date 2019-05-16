8903b1b7dac907ca0d7d22424c1060eb6ebabbf1
yagami light <halysl0817@gmail.com>
2019-05-16 11:43:27 +0800

    add 利用闭包实现单方法的类转换为函数

---
+++ src/study_cookbook/函数/将单方法的类转换为函数.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# 利用闭包特性
+
+from urllib.request import urlopen
+
+
+class UrlTemplate:
+    def __init__(self, template):
+        self.template = template
+
+    def open(self, **kwargs):
+        return urlopen(self.template.format_map(kwargs))
+
+
+def urltemplate(template):
+    def opener(**kwargs):
+        return urlopen(template.format_map(kwargs))
+    return opener
+
+if __name__ == "__main__":
+    base_url = "https://www.google.com/search?q={search}"
+    yahoo = UrlTemplate(base_url)
+    for line in yahoo.open(search='ces'):
+        print(line.decode('utf-8'))
+    
+    yahoo = urltemplate(base_url)
+    for line in yahoo(search="qwer"):
+        print(line.decode('utf-8'))
