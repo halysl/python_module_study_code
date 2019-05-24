5a76a8d77677e2ee23247000fa4016b322782a8a
yagami light <halysl0817@gmail.com>
2019-05-24 15:10:20 +0800

    add 属性代理访问

--- src/study_cookbook/类与对象/属性的代理访问.py
+++ src/study_cookbook/类与对象/属性的代理访问.py
@@ -1,2 +1,101 @@
 # -*- coding: utf-8 -*-
 # https://python3-cookbook.readthedocs.io/zh_CN/latest/c08/p15_delegating_attribute_access.html
+# 代理是一种编程模式，它将某个操作转移给另外一个对象来实现。
+
+
+class A:
+    def spam(self, x):
+        print("A: {}".format(x))
+
+    def foo(self):
+        pass
+
+
+class B1:
+    """简单的代理"""
+
+    def __init__(self):
+        self._a = A()
+
+    def spam(self, x):
+        # Delegate to the internal self._a instance
+        return self._a.spam(x)
+
+    def foo(self):
+        # Delegate to the internal self._a instance
+        return self._a.foo()
+
+    def bar(self):
+        pass
+
+
+class B2:
+    """使用__getattr__的代理，代理方法比较多时候"""
+
+    def __init__(self):
+        self._a = A()
+
+    def bar(self):
+        pass
+
+    # Expose all of the methods defined on class A
+    def __getattr__(self, name):
+        """这个方法在访问的attribute不存在的时候被调用
+        the __getattr__() method is actually a fallback method
+        that only gets called when an attribute is not found"""
+        return getattr(self._a, name)
+
+
+def example_daili_mode():
+    b1 = B1()
+    b2 = B2()
+    b1.spam(3)
+    b2.spam(4)
+
+
+class Proxy:
+    def __init__(self, obj):
+        self._obj = obj
+
+    # Delegate attribute lookup to internal obj
+    def __getattr__(self, name):
+        print('getattr:', name)
+        return getattr(self._obj, name)
+
+    # Delegate attribute assignment
+    def __setattr__(self, name, value):
+        if name.startswith('_'):
+            super().__setattr__(name, value)
+        else:
+            print('setattr:', name, value)
+            setattr(self._obj, name, value)
+
+    # Delegate attribute deletion
+    def __delattr__(self, name):
+        if name.startswith('_'):
+            super().__delattr__(name)
+        else:
+            print('delattr:', name)
+            delattr(self._obj, name)
+
+
+class Spam:
+    def __init__(self, x):
+        self.x = x
+
+    def bar(self, y):
+        print('Spam.bar:', self.x, y)
+
+
+def example_spam():
+    s = Spam(2)
+    # Create a proxy around it
+    p = Proxy(s)
+    # Access the proxy
+    print(p.x)  # Outputs 2
+    p.bar(3)  # Outputs "Spam.bar: 2 3"
+    p.x = 37  # Changes s.x to 37
+
+if __name__ == "__main__":
+    example_daili_mode()
+    example_spam()
