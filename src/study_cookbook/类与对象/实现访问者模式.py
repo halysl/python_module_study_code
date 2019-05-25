3011af6aca97def0ae4237b09ef61ee9f1326694
yagami light <halysl0817@gmail.com>
2019-05-25 17:08:01 +0800

    add 访问者模式

---
+++ src/study_cookbook/类与对象/实现访问者模式.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# 通过 getattr 实现
+
+
+class NodeVisitor:
+    def visit(self, node):
+        methname = 'visit_' + type(node).__name__
+        meth = getattr(self, methname, None)
+        if meth is None:
+            meth = self.generic_visit
+        return meth(node)
+
+    def generic_visit(self, node):
+        raise RuntimeError('No {} method'.format('visit_' + type(node).__name__))
+
+
+class Evaluator(NodeVisitor):
+    def visit_Number(self, node):
+        return node.value
+
+    def visit_Add(self, node):
+        return self.visit(node.left) + self.visit(node.right)
+
+    def visit_Sub(self, node):
+        return self.visit(node.left) - self.visit(node.right)
+
+    def visit_Mul(self, node):
+        return self.visit(node.left) * self.visit(node.right)
+
+    def visit_Div(self, node):
+        return self.visit(node.left) / self.visit(node.right)
+
+    def visit_Negate(self, node):
+        return -node.operand
+
+
+def example_fangwenzhemoshi1():
+    e = Evaluator()
+    e.visit(Number(3))
+
+if __name__ == "__main__":
+    example_fangwenzhemoshi1()
