351122b6d013928140ebc2ea0cf252c7912bcb9d
yagami light <halysl0817@gmail.com>
2019-05-27 10:08:39 +0800

    add 装饰器

---
+++ src/study_cookbook/元编程/装饰器.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import time
+from functools import wraps
+
+
+def timethis(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(func.__name__, end - start)
+        return result
+    return wrapper
+
+
+@timethis
+def countdown(n):
+    while n > 0:
+        n -= 1
+
+
+if __name__ == "__main__":
+    countdown(10000)
+    countdown(1000000)
+    countdown(10000000)
+    countdown(100000000)
