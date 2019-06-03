16bb2a19775ea4d8964e41a55762f2902c05d715
yagami light <halysl0817@gmail.com>
2019-06-04 00:10:07 +0800

    add 将装饰器定义为类的一部分

--- src/study_cookbook/元编程/将装饰器定义为类的一部分.py
+++ src/study_cookbook/元编程/将装饰器定义为类的一部分.py
@@ -6,11 +6,12 @@ from functools import wraps
 class A(object):
     def decorator1(self, func):
         @wraps(func)
+        # 注意 wrapper() 函数没有 self
         def wrapper(*args, **kwargs):
             print("Decorator1")
             return func(*args, **kwargs)
         return wrapper
-    
+
     @classmethod
     def decorator2(self, func):
         @wraps(func)
@@ -20,14 +21,23 @@ class A(object):
         return wrapper
 
 
+class B(A):
+    # 虽然继承了 A，但是还没实例化，所以不能直接 decorator2
+    @A.decorator2
+    def test():
+        pass
+
+
 if __name__ == "__main__":
     a = A()
+
     @a.decorator1
     def spam():
         print("hello")
+
     @A.decorator2
     def grok():
         print("world")
-    
+
     spam()
     grok()
