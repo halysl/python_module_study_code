f10d9cec9ffe0f8af8c1a1832fd806a895b14119
yagami light <halysl0817@gmail.com>
2019-06-04 00:04:21 +0800

    add 使用装饰器进行类型检测：增加解释

--- src/study_cookbook/元编程/利用装饰器强制函数上的类型检查.py
+++ src/study_cookbook/元编程/利用装饰器强制函数上的类型检查.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# https://python3-cookbook.readthedocs.io/zh_CN/latest/c09/p07_enforcing_type_check_on_function_using_decorator.html
-# 周末看
+
 from inspect import signature
 from functools import wraps
 
@@ -13,11 +12,14 @@ def typeassert(*ty_args, **ty_kwargs):
             return func
 
         # Map function argument names to supplied types
+        # signature的用法是提取函数签名，可看方法 test_signature()
         sig = signature(func)
+        # 然后对函数签名进行类型的绑定，.arguments是将 BoundArguments 对象转为一个 OrderedDict
         bound_types = sig.bind_partial(*ty_args, **ty_kwargs).arguments
 
         @wraps(func)
         def wrapper(*args, **kwargs):
+            # bound_values 就是将参数具体的包给函数签名
             bound_values = sig.bind(*args, **kwargs)
             # Enforce type assertions across supplied arguments
             for name, value in bound_values.arguments.items():
@@ -32,9 +34,24 @@ def typeassert(*ty_args, **ty_kwargs):
 
 
 @typeassert(int, int)
-def add(x, y):
-    return x + y
+def add(x, z):
+    return x + z
+
+
+def spam(x, y, z=10):
+    pass
+
+
+def test_signature():
+    sig = signature(spam)
+    print(sig)
+    print(sig.parameters)
+    bond_type = sig.bind_partial((int, float), int)
+    print(bond_type)
+    bond_type = bond_type.arguments
+    print(bond_type)
 
 if __name__ == "__main__":
-    add(2, 3)
-    add(1, "hello")
+    res = add(2, 3)
+    print(res)
+    test_signature()
