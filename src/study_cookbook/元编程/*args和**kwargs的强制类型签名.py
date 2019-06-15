e25a94be7e8735d3f5d01f4b06ae262b72fae607
yagami light <halysl0817@gmail.com>
2019-06-15 09:53:47 +0800

    add 对*args和**kwargs对象进行类型签名

---
+++ src/study_cookbook/元编程/*args和**kwargs的强制类型签名.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from inspect import Signature, Parameter, signature
+
+# 对每一个参数指定位置和默认值，生成一个签名对象
+params = [Parameter("x", Parameter.POSITIONAL_OR_KEYWORD),
+          Parameter("y", Parameter.POSITIONAL_OR_KEYWORD, default=42),
+          Parameter("z", Parameter.KEYWORD_ONLY, default=None)]
+print(params)
+sig = Signature(params)
+print(sig, type(sig))
+
+
+def func(*args, **kwargs):
+    print('\n')
+    bound_values = sig.bind(*args, **kwargs)
+    for name, value in bound_values.arguments.items():
+        print(name, value)
+
+
+def make_sig(*names):
+    parms = [Parameter(name, Parameter.POSITIONAL_OR_KEYWORD)
+             for name in names]
+    return Signature(parms)
+
+
+class Structure:
+    __signature__ = make_sig()
+
+    def __init__(self, *args, **kwargs):
+        bound_values = self.__signature__.bind(*args, **kwargs)
+        for name, value in bound_values.arguments.items():
+            setattr(self, name, value)
+
+
+class Stock(Structure):
+    __signature__ = make_sig('name', 'shares', 'price')
+
+
+class Point(Structure):
+    __signature__ = make_sig('x', 'y')
+
+if __name__ == "__main__":
+    func(1, 2, z=3)
+    func(1)
+    func(1, 3)
+    print(signature(Stock))
+    print(signature(Point))
+    s1 = Stock('ACME', 100, 490.1)
+    print(s1.__dict__)
