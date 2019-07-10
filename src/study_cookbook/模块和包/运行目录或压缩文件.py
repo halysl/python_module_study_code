fecfab79f9dba0b7d3b1a6a49fec3def02f070dc
yagami light <halysl0817@gmail.com>
2019-07-10 21:17:44 +0800

    add 运行目录或者压缩文件

---
+++ src/study_cookbook/模块和包/运行目录或压缩文件.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+myapplication/
+    spam.py
+    bar.py
+    grok.py
+    __main__.py
+"""
+
+"""
+bash % python3 myapplication
+"""
+
+"""
+bash % ls
+spam.py bar.py grok.py __main__.py
+bash % zip -r myapp.zip *.py
+bash % python3 myapp.zip
+... output from __main__.py ...
+"""
