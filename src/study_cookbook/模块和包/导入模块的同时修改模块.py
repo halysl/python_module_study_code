97668ae054bf324fdadcef7c2d4894a3800b1f4c
yagami light <halysl0817@gmail.com>
2019-07-12 20:04:09 +0800

    add 两个暂时看不懂的操作

---
+++ src/study_cookbook/模块和包/导入模块的同时修改模块.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# https://python3-cookbook.readthedocs.io/zh_CN/latest/c10/p12_patching_modules_on_import.html
---
+++ src/study_cookbook/模块和包/通过钩子远程加载模块.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# https://python3-cookbook.readthedocs.io/zh_CN/latest/c10/p11_load_modules_from_remote_machine_by_hooks.html
