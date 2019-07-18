f2217747ea708a5e2f825c8fcae2f46bc263c6c6
yagami light <halysl0817@gmail.com>
2019-07-18 16:30:11 +0800

    修改文件位置

--- src/study_cookbook/11网络与web编程/发送与接收大型数组.py
+++ src/study_cookbook/11网络与web编程/发送与接收大型数组.py
@@ -13,3 +13,21 @@ def recv_into(arr, source):
     while len(view):
         nrecv = source.recv_into(view)
         view = view[nrecv:]
+
+"""
+create a server
+>>> from socket import *
+>>> s = socket(AF_INET, SOCK_STREAM)
+>>> s.bind(('', 25000))
+>>> s.listen(1)
+>>> c,a = s.accept()
+>>>
+"""
+
+"""
+create a client
+>>> from socket import *
+>>> c = socket(AF_INET, SOCK_STREAM)
+>>> c.connect(('localhost', 25000))
+>>>
+"""
