180cc3031595763844a4dec86e7a0bf35ce91209
yagami light <halysl0817@gmail.com>
2019-08-07 17:52:48 +0800

    add upup

---
+++ src/study_cookbook/12并发编程/多个线程队列轮询.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# https://python3-cookbook.readthedocs.io/zh_CN/latest/c12/p13_polling_multiple_thread_queues.html
---
+++ src/study_cookbook/12并发编程/定义一个Actor任务.py
@@ -0,0 +1,2 @@
+# -*- codinf: utf-8 -*-
+# https://python3-cookbook.readthedocs.io/zh_CN/latest/c12/p10_defining_an_actor_task.html
