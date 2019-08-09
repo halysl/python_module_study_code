0929fbad952360f2c5345a27d7f6a05ca297831f
yagami light <halysl0817@gmail.com>
2019-08-09 18:02:21 +0800

    add 解析命令行选项

--- src/study_cookbook/13脚本编程与系统管理/解析命令行选项.py
+++ src/study_cookbook/13脚本编程与系统管理/解析命令行选项.py
@@ -1,1 +1,76 @@
 # -*- coding: utf-8 -*-
+import argparse
+parser = argparse.ArgumentParser(description='Search some files')
+
+parser.add_argument(dest='filenames', metavar='filename', nargs='*')
+
+parser.add_argument('-p', '--pat', metavar='pattern', required=True,
+                    dest='patterns', action='append',
+                    help='text pattern to search for')
+
+parser.add_argument('-v', dest='verbose', action='store_true',
+                    help='verbose mode')
+
+parser.add_argument('-o', dest='outfile', action='store',
+                    help='output file')
+
+parser.add_argument('--speed', dest='speed', action='store',
+                    choices={'slow', 'fast'}, default='slow',
+                    help='search speed')
+
+args = parser.parse_args()
+
+# Output the collected arguments
+print(args.filenames)
+print(args.patterns)
+print(args.verbose)
+print(args.outfile)
+print(args.speed)
+
+"""
+bash % python3 search.py -h
+usage: search.py [-h] [-p pattern] [-v] [-o OUTFILE] [--speed {slow,fast}]
+                 [filename [filename ...]]
+
+Search some files
+
+positional arguments:
+  filename
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -p pattern, --pat pattern
+                        text pattern to search for
+  -v                    verbose mode
+  -o OUTFILE            output file
+  --speed {slow,fast}   search speed
+"""
+
+"""
+bash % python3 search.py foo.txt bar.txt
+usage: search.py [-h] -p pattern [-v] [-o OUTFILE] [--speed {fast,slow}]
+                 [filename [filename ...]]
+search.py: error: the following arguments are required: -p/--pat
+
+bash % python3 search.py -v -p spam --pat=eggs foo.txt bar.txt
+filenames = ['foo.txt', 'bar.txt']
+patterns  = ['spam', 'eggs']
+verbose   = True
+outfile   = None
+speed     = slow
+
+bash % python3 search.py -v -p spam --pat=eggs foo.txt bar.txt -o results
+filenames = ['foo.txt', 'bar.txt']
+patterns  = ['spam', 'eggs']
+verbose   = True
+outfile   = results
+speed     = slow
+
+bash % python3 search.py -v -p spam --pat=eggs foo.txt bar.txt -o results \
+             --speed=fast
+filenames = ['foo.txt', 'bar.txt']
+patterns  = ['spam', 'eggs']
+verbose   = True
+outfile   = results
+speed     = fast
+"""
