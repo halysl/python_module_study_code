3670f748a08f5ecaa7ee8fa2da46182783d64c25
yagami light <halysl0817@gmail.com>
2019-08-21 19:02:56 +0800

    add prompt toolkit first repl

---
+++ src/study_prompt_toolkit/first_repl.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from prompt_toolkit import prompt
+from prompt_toolkit.history import InMemoryHistory
+
+
+def main():
+    history = InMemoryHistory()
+
+    while True:
+        text = prompt("> ", history=history)
+        if text in ["quit", "exit"]:
+            break
+        print("You entered:", text)
+    print("GoodBye!")
+
+
+if __name__ == "__main__":
+    main()
