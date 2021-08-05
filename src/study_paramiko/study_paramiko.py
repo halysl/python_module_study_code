74c22f076dda40baffb7daea551e10700f0020a9
yagami light <halysl0817@gmail.com>
2021-08-05 16:21:41 +0800

    refactor: 更新依赖

--- src/study_paramiko/study_paramiko.py
+++ src/study_paramiko/study_paramiko.py
@@ -8,7 +8,7 @@ ssh = paramiko.SSHClient()
 # 这行代码的作用是允许连接不在know_hosts文件中的主机
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 # connect方法，分别输入ip、端口、账户、密码（或秘钥文件）
-ssh.connect('10.10.100.75', 22, 'root', 'xxxxxxx')
+ssh.connect('172.16.0.1', 22, 'A', 'B')
 # 通过stdin,stdout,stderr获得exec_command（）执行命令后的返回结果
 # 命令执行出错并不会抛出异常，所以，对于命令出错需要根据自己的需求进行相应的处理
 stdin, stdout, stderr = ssh.exec_command('ls /')
@@ -19,22 +19,6 @@ stdin, stdout, stderr = ssh.exec_command('ls /')
 # u'oswbb\n', u'proc\n', u'qdata.log\n', u'root\n', u'run\n',
 # u'sbin\n', u'srv\n', u'sys\n', u'tmp\n', u'usr\n', u'var\n']
 # stderr []
-print stdout.read()
-print stderr.read()
+print(stdout.read())
+print(stderr.read())
 
-stdin, stdout, stderr = ssh.exec_command('cat sad')
-# stdout []
-# stderr [u'cat: sad: No such file or directory\n']
-print stdout.read()
-print stderr.read()
-
-# 创建一个sftp对象，可以上传下载文件
-# 另一种创建的方法为sftp = paramiko.SFTPClient.from_transport(ssh.get_transport())
-sftp = ssh.open_sftp()
-
-# get下载文件，需要三个参数，self，目标文件， 下载后的文件位置
-sftp.get('/var/log/qdata_test.log', './a.log')
-# put上传文件，需要三个参数，self，待上传文件，上传后的位置
-sftp.put('a.log', '/a.log')
-
-ssh.close()
